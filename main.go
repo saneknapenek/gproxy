d8101de595c06a54cf33ff68067d63e1d0d79151
saneknapenek <AlEkSaNdR.KoLeSnIkOv22@yandex.ru>
2024-10-18 02:22:21 +0300

    draft

--- main.go
+++ main.go
@@ -1,104 +1,30 @@
-package gproxy
+package main
+
 
 import (
-	"bytes"
-	"crypto/tls"
-	"flag"
-	"fmt"
-	"io"
-	"log"
-	"net"
 	"net/http"
-	"time"
-	"os"
+	"crypto/tls"
+	"log/slog"
+	"fmt"
+	"strings"
+
+	"github.com/saneknapenek/clog"
 )
 
-func handleTunneling(w http.ResponseWriter, r *http.Request) {
-    dest_conn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusServiceUnavailable)
-        return
-    }
-
-    w.WriteHeader(http.StatusOK)
-
-    hijacker, ok := w.(http.Hijacker)
-    if !ok {
-        http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
-        return
-    }
-
-    client_conn, _, err := hijacker.Hijack()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusServiceUnavailable)
-    }
-
-	if err := os.WriteFile("file", []byte("tunneling"), os.ModeAppend); err != nil {
-		log.Fatalf("error writing to file: %s", err.Error())
-	}
-
-    go transfer(dest_conn, client_conn)
-    go transfer(client_conn, dest_conn)
-}
-
-func transfer(destination io.WriteCloser, source io.ReadCloser) {
-    defer destination.Close()
-    defer source.Close()
-    io.Copy(destination, source)
-}
-
-func handleHTTP(w http.ResponseWriter, req *http.Request) {
-    resp, err := http.DefaultTransport.RoundTrip(req)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusServiceUnavailable)
-        return
-    }
-
-    defer resp.Body.Close()
-
-    copyHeader(w.Header(), resp.Header)
-    w.WriteHeader(resp.StatusCode)
-
-	b := bytes.NewBuffer(nil)
-
-	if err := os.WriteFile("file", b.Bytes(), os.ModeAppend); err != nil {
-		log.Fatalf("error writing to file: %s", err.Error())
-	}
-
-	fmt.Println(b)
-
-    io.Copy(w, b)
-}
-
-func copyHeader(dst, src http.Header) {
-    for k, vv := range src {
-        for _, v := range vv {
-            dst.Add(k, v)
-        }
-    }
-}
 
 func main() {
-    var pemPath string
-    flag.StringVar(&pemPath, "pem", "server.pem", "path to pem file")
 
-    var keyPath string
-    flag.StringVar(&keyPath, "key", "server.key", "path to key file")
+	log := clog.InitLogger(clog.EnvDev, nil)
 
-    // var proto string
-    // flag.StringVar(&proto, "proto", "https", "Proxy protocol (http or https)")
+	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+    if err != nil {
+        log.Error(err.Error())
+        return
+    }
 
-    flag.Parse()
-
-    // if proto != "http" && proto != "https" {
-    //     log.Fatal("Protocol must be either http or https")
-    // }
-
-    server := &http.Server{
-        Addr: ":8888",
-        Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            if r.Method == http.MethodConnect {
-                handleTunneling(w, r)
+	server := &http.Server{
+        Addr: ":8888", Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { if r.Method == http.MethodConnect {
+                handleTunneling(*log, w, r)
             } else {
                 handleHTTP(w, r)
             }
@@ -107,11 +33,88 @@ func main() {
         TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
     }
 
-	// serverTLS := 
-
-//     if proto == "http" {
-//         log.Fatal(server.ListenAndServe())
-//     } else {
-//         log.Fatal(server.ListenAndServeTLS(pemPath, keyPath))
-//     }
+	if err := server.ListenAndServe(); err != nil {
+		log.Error(err.Error())
+		return
+	}
 }
+
+
+func handleTunneling(log slog.Logger, w http.ResponseWriter, r *http.Request) {
+    // dest_conn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+    // if err != nil {
+    //     http.Error(w, err.Error(), http.StatusServiceUnavailable)
+    //     return
+    // }
+
+    // w.WriteHeader(http.StatusOK)
+
+	strHeaders, err := headerToString(r.Header)
+	if err != nil {
+		log.Error("error parsing headers:", err.Error())
+	} else {
+		log.Debug("", slog.String("Headers", strHeaders))
+	}
+
+	hijacker, ok := w.(http.Hijacker)
+    if !ok {
+        http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		log.Debug("Hijacking not supported", slog.String("host", r.Host))
+        return
+    }
+
+    clientConn, _, err := hijacker.Hijack()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Debug("connection capture error", slog.String("error", err.Error()))
+        return
+    }
+
+    defer clientConn.Close()
+    defer destConn.Close()
+
+	tlsConfig := &tls.Config{
+        InsecureSkipVerify: true,
+    }
+
+    tlsConn := tls.Server(clientConn, tlsConfig)
+
+    err = tlsConn.Handshake()
+    if err != nil {
+        http.Error(w, "Failed to complete TLS handshake: "+err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Оборачиваем клиентское соединение в TLS
+    tlsConn := tls.Server(clientConn, tlsConfig)
+}
+
+func headerToString(headers http.Header) (string, error) {
+    var sb strings.Builder
+
+    for key, values := range headers {
+        for _, value := range values {
+            _, err := sb.WriteString(fmt.Sprintf("%s: %s\n", key, value))
+            if err != nil {
+                return "", err
+            }
+        }
+    }
+
+    return sb.String(), nil
+}
+
+// func headerToSlogSlice(headers http.Header) ([]slog.Attr, error) {
+// 	hCount := len(headers)
+
+//     result := make([]slog.Attr, 0, hCount)
+
+//     for key, values := range headers {
+//         for _, value := range values {
+//             // Добавляем заголовок в срез в формате slog.String
+//             result = append(result, slog.String(key, value))
+//         }
+//     }
+
+//     return result, nil
+// }
--- open.go
+++
@@ -1,3 +0,0 @@
-// package gproxy
-
-
